95294b6f7d49256b787dfb3f45cadedfdc263f68
Koozie24 <jake.mckenzie2017@gmail.com>
2024-06-12 11:56:17 -0400

    define struct Node

--- l_list_1.cpp
+++ l_list_1.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
+/* The purpose of this program is to demonstrate an implementation of the linked list data structure. 
+I reference the following link: https://www.w3schools.in/data-structures/linked-list */
+
+//define a node that contains data and pointer to next node
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int data) : data(data), next(nullptr){}
+};
+
 int main(){
 
     return 0;
